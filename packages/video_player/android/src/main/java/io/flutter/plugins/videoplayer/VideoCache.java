4f0f322fe870dfec7868cdc25ed4ff0074d8316a
Markus Paschke <markus.paschke@web.de>
2019-05-31 17:05:19 +0200

    new

--- packages/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoCache.java
+++ packages/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoCache.java
@@ -8,7 +8,7 @@ public class VideoCache {
     private static SimpleCache sDownloadCache;
 
     public static SimpleCache getInstance() {
-        if (sDownloadCache == null) sDownloadCache = new SimpleCache(new File("/storage/emulated/0/Movies/"), new LeastRecentlyUsedCacheEvictor(100 * 1024 * 1024));
+        if (sDownloadCache == null) sDownloadCache = new SimpleCache(new File("/storage/emulated/0/Movies/"), new LeastRecentlyUsedCacheEvictor(300 * 1024 * 1024));
         return sDownloadCache;
     }
 }
